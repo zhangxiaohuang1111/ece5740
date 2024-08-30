b8b347e936a4983d9459945f464ebf849fd3d33e
fangzhong-lyu <soxehli@gmail.com>
2024-08-30 13:47:45 -0400

    staff update from: ece4750-lab1-release-0.0-932-g23e2479 [ci skip]

---
+++ sim/lab1_imul/IntMulAlt.v
@@ -0,0 +1,70 @@
+//=========================================================================
+// Integer Multiplier Variable-Latency Implementation
+//=========================================================================
+
+`ifndef LAB1_IMUL_INT_MUL_ALT_V
+`define LAB1_IMUL_INT_MUL_ALT_V
+
+`include "vc/trace.v"
+
+// ''' LAB TASK ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+// Define datapath and control unit here.
+// '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+//=========================================================================
+// Integer Multiplier Variable-Latency Implementation
+//=========================================================================
+
+module lab1_imul_IntMulAlt
+(
+  input  logic        clk,
+  input  logic        reset,
+
+  input  logic        istream_val,
+  output logic        istream_rdy,
+  input  logic [63:0] istream_msg,
+
+  output logic        ostream_val,
+  input  logic        ostream_rdy,
+  output logic [31:0] ostream_msg
+);
+
+  // ''' LAB TASK ''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  // Instantiate datapath and control models here and then connect them
+  // together.
+  // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+  //----------------------------------------------------------------------
+  // Line Tracing
+  //----------------------------------------------------------------------
+
+  `ifndef SYNTHESIS
+
+  logic [`VC_TRACE_NBITS-1:0] str;
+  `VC_TRACE_BEGIN
+  begin
+
+    $sformat( str, "%x", istream_msg );
+    vc_trace.append_val_rdy_str( trace_str, istream_val, istream_rdy, str );
+
+    vc_trace.append_str( trace_str, "(" );
+
+    // ''' LAB TASK ''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    // Add additional line tracing using the helper tasks for
+    // internal state including the current FSM state.
+    // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+    vc_trace.append_str( trace_str, ")" );
+
+    $sformat( str, "%x", ostream_msg );
+    vc_trace.append_val_rdy_str( trace_str, ostream_val, ostream_rdy, str );
+
+  end
+  `VC_TRACE_END
+
+  `endif /* SYNTHESIS */
+
+endmodule
+
+`endif /* LAB1_IMUL_INT_MUL_ALT_V */
+
---
+++ sim/lab1_imul/test/IntMulFL_test.py
@@ -0,0 +1,107 @@
+#=========================================================================
+# IntMulFL_test
+#=========================================================================
+
+import pytest
+
+from random import randint
+
+from pymtl3 import *
+from pymtl3.stdlib.test_utils import mk_test_case_table, run_sim
+from pymtl3.stdlib.stream import StreamSourceFL, StreamSinkFL
+
+from lab1_imul.IntMulFL import IntMulFL
+
+#-------------------------------------------------------------------------
+# TestHarness
+#-------------------------------------------------------------------------
+
+class TestHarness( Component ):
+
+  def construct( s, imul ):
+
+    # Instantiate models
+
+    s.src  = StreamSourceFL( Bits64 )
+    s.sink = StreamSinkFL( Bits32 )
+    s.imul = imul
+
+    # Connect
+
+    s.src.ostream  //= s.imul.istream
+    s.imul.ostream //= s.sink.istream
+
+  def done( s ):
+    return s.src.done() and s.sink.done()
+
+  def line_trace( s ):
+    return s.src.line_trace() + " > " + s.imul.line_trace() + " > " + s.sink.line_trace()
+
+#-------------------------------------------------------------------------
+# mk_imsg/mk_omsg
+#-------------------------------------------------------------------------
+
+# Make input message, truncate ints to ensure they fit in 32 bits.
+
+def mk_imsg( a, b ):
+  return concat( Bits32( a, trunc_int=True ), Bits32( b, trunc_int=True ) )
+
+# Make output message, truncate ints to ensure they fit in 32 bits.
+
+def mk_omsg( a ):
+  return Bits32( a, trunc_int=True )
+
+#----------------------------------------------------------------------
+# Test Case: small positive * positive
+#----------------------------------------------------------------------
+
+small_pos_pos_msgs = [
+  mk_imsg(  2,  3 ), mk_omsg(   6 ),
+  mk_imsg(  4,  5 ), mk_omsg(  20 ),
+  mk_imsg(  3,  4 ), mk_omsg(  12 ),
+  mk_imsg( 10, 13 ), mk_omsg( 130 ),
+  mk_imsg(  8,  7 ), mk_omsg(  56 ),
+]
+
+# ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Define additional lists of input/output messages to create
+# additional directed and random test cases.
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+#-------------------------------------------------------------------------
+# Test Case Table
+#-------------------------------------------------------------------------
+
+test_case_table = mk_test_case_table([
+  (                      "msgs                   src_delay sink_delay"),
+  [ "small_pos_pos",     small_pos_pos_msgs,     0,        0          ],
+
+  # ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  # Add more rows to the test case table to leverage the additional lists
+  # of request/response messages defined above, but also to test
+  # different source/sink random delays.
+  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+])
+
+#-------------------------------------------------------------------------
+# TestHarness
+#-------------------------------------------------------------------------
+
+@pytest.mark.parametrize( **test_case_table )
+def test( test_params, cmdline_opts ):
+
+  th = TestHarness( IntMulFL() )
+
+  th.set_param("top.src.construct",
+    msgs=test_params.msgs[::2],
+    initial_delay=test_params.src_delay+3,
+    interval_delay=test_params.src_delay )
+
+  th.set_param("top.sink.construct",
+    msgs=test_params.msgs[1::2],
+    initial_delay=test_params.sink_delay+3,
+    interval_delay=test_params.sink_delay )
+
+  run_sim( th, cmdline_opts, duts=['imul'] )
+
