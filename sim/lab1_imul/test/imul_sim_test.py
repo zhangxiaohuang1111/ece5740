1bed9287faa24700525a1bc1f396e2a01cf9c320
zh476 <zh476@cornell.edu>
2024-09-18 21:07:04 -0400

    1

--- sim/lab1_imul/test/imul_sim_test.py
+++ sim/lab1_imul/test/imul_sim_test.py
@@ -9,8 +9,8 @@ import os
 from subprocess import check_call, CalledProcessError
 from itertools  import product
 
-impls  = [ "fl" ]
-inputs = [ "small" ]
+impls  = [ "fl" ,"base", "alt" ]
+inputs = [ "small","random" ]
 
 # ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # Once you get your baseline and alternative design passing all of your
