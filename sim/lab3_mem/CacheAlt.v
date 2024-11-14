46ae37f57108fc050f9171b89045deeeaca2a8f1
zh476 <zh476@cornell.edu>
2024-11-14 09:30:28 -0500

    1

--- sim/lab3_mem/CacheAlt.v
+++ sim/lab3_mem/CacheAlt.v
@@ -158,58 +158,6 @@ module lab3_mem_CacheAlt
       default:                      vc_trace.append_str( trace_str, "? " );
 
     endcase
-
-    /*
-    vc_trace.append_str( trace_str, " " );
-
-    // Use a "hit" signal in the control unit to display h/m
-
-    if ( ctrl.state_reg == ctrl.STATE_TAG_CHECK ) begin
-      if ( ctrl.hit_TC )
-        vc_trace.append_str( trace_str, "h" );
-      else
-        vc_trace.append_str( trace_str, "m" );
-    end
-    else
-      vc_trace.append_str( trace_str, " " );
-
-    // Way 0: Display all valid tags, show dirty bits with ; symbol
-
-    vc_trace.append_str( trace_str, "[" );
-    for ( i = 0; i < 8; i = i + 1 ) begin
-      if ( !ctrl.valid_bits_way0.rfile[i] )
-        vc_trace.append_str( trace_str, "   " );
-      else begin
-        $sformat( str, "%x", dpath.tag_array_way0.mem[i][7:0] );
-        vc_trace.append_str( trace_str, str );
-        if      (  ctrl.dirty_bits.rfile[i] && (ctrl.use_bits.rfile[i] != 1'b0) ) vc_trace.append_str( trace_str, "," );
-        else if ( !ctrl.dirty_bits.rfile[i] && (ctrl.use_bits.rfile[i] == 1'b0) ) vc_trace.append_str( trace_str, "." );
-        else if (  ctrl.dirty_bits.rfile[i] && (ctrl.use_bits.rfile[i] == 1'b0) ) vc_trace.append_str( trace_str, ";" );
-        else                                                              vc_trace.append_str( trace_str, " " );
-      end
-    end
-    vc_trace.append_str( trace_str, "]" );
-
-    // Way 1: Display all valid tags, show dirty bits with ; symbol
-
-    vc_trace.append_str( trace_str, "[" );
-    for ( i = 0; i < 8; i = i + 1 ) begin
-      if ( !ctrl.valid_bits_way1.rfile[i] )
-        vc_trace.append_str( trace_str, "   " );
-      else begin
-        $sformat( str, "%x", dpath.tag_array_way1.mem[i][7:0] );
-        vc_trace.append_str( trace_str, str );
-        if      (  ctrl.dirty_bits.rfile[8+i] && (ctrl.use_bits.rfile[i] != 1'b1) ) vc_trace.append_str( trace_str, "," );
-        else if ( !ctrl.dirty_bits.rfile[8+i] && (ctrl.use_bits.rfile[i] == 1'b1) ) vc_trace.append_str( trace_str, "." );
-        else if (  ctrl.dirty_bits.rfile[8+i] && (ctrl.use_bits.rfile[i] == 1'b1) ) vc_trace.append_str( trace_str, ";" );
-        else                                                                vc_trace.append_str( trace_str, " " );
-      end
-    end
-    vc_trace.append_str( trace_str, "]" );
-
-    vc_trace.append_str( trace_str, ")" );
-    */
-
   end
   `VC_TRACE_END
 
