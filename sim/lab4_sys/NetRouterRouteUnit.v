3a93134088a601346d04f45fae0ed09b6c0ea1db
zh476 <zh476@cornell.edu>
2024-12-04 00:45:26 -0500

    routeunit

--- sim/lab4_sys/NetRouterRouteUnit.v
+++ sim/lab4_sys/NetRouterRouteUnit.v
@@ -38,6 +38,45 @@ module lab4_sys_NetRouterRouteUnit
   net_msg_hdr_t istream_msg_hdr;
   assign istream_msg_hdr = istream_msg[`VC_NET_MSGS_HDR(p_msg_nbits)];
 
+  // Pass the message to all output streams
+  assign ostream_msg[0] = istream_msg;
+  assign ostream_msg[1] = istream_msg;
+  assign ostream_msg[2] = istream_msg;
+  logic [1:0] clockwise_dist;
+  logic [1:0] counterclockwise_dist;
+
+  always_comb begin
+
+    // Default outputs
+    istream_rdy  = 0;
+    ostream_val[0] = 0;
+    ostream_val[1] = 0;
+    ostream_val[2] = 0;
+    clockwise_dist = 0;
+    counterclockwise_dist = 0;
+
+    if (istream_val) begin
+      // Calculate distance to destination
+      clockwise_dist = (istream_msg_hdr.dest - router_id) & 2'b11;      // Wrap around modulo 4
+      counterclockwise_dist = (router_id - istream_msg_hdr.dest) & 2'b11; // Wrap around modulo 4
+
+      // Determine shortest path
+      if (istream_msg_hdr.dest == router_id) begin
+        // Destination is the current router
+        istream_rdy = ostream_rdy[2];
+        ostream_val[2] = 1;
+      end else if (clockwise_dist < counterclockwise_dist) begin
+        // Route clockwise (output 1)
+        istream_rdy = ostream_rdy[1];
+        ostream_val[1] = 1;
+      end else begin
+        // Route counterclockwise (output 0)
+        istream_rdy = ostream_rdy[0];
+        ostream_val[0] = 1;
+      end
+    end
+  end
+
   //''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   // Implement route unit logic
   //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
